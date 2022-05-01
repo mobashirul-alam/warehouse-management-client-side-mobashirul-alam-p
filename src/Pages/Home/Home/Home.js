401b0d09623081d85db50414942b407d0361313b
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-05-01 13:24:30 +0600

    folder structure created & firebase app initialized

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Fitness Pro Equipment Warehouse!!!</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SocialLogin = () => {
+    return (
+        <div>
+            <h1>This is social login page</h1>
+        </div>
+    );
+};
+
+export default SocialLogin;
