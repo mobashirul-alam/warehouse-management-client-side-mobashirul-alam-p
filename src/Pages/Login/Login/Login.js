f11a7d1ba50ec05f84877013f63d334d929233f7
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-05-06 21:38:15 +0600

    functional registration implemented

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -11,12 +11,12 @@ const Login = () => {
                 <Form className='p-3'>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control type="email" placeholder="Enter email" required />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" required />
                     </Form.Group>
                     <Button variant="secondary" type="submit">
                         Login
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,32 +1,67 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 
 const Register = () => {
+    const nameRef = useRef('');
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+    const [agreePolicyTerms, setAgreePolicyTerms] = useState(false);
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        await createUserWithEmailAndPassword(email, password);
+        await updateProfile({ displayName: name })
+    }
+
+    if (user) {
+        navigate('/home');
+    }
+
     return (
         <div className='container mb-5'>
             <h1 className='mt-2 mb-3 display-6 text-center'>Create your account</h1>
             <div className='border border-success p-3 rounded-3'>
-                <Form className='p-3'>
+                <Form onSubmit={handleSubmit} className='p-3'>
                     <Form.Group className="mb-3">
                         <Form.Label>Your Name</Form.Label>
-                        <Form.Control type="text" placeholder="Your Name" />
+                        <Form.Control ref={nameRef} type="text" placeholder="Your Name" required />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                     </Form.Group>
                     <Form.Group className="mb-3 d-flex" controlId="formBasicCheckbox">
                         <Form.Check
+                            onClick={() => setAgreePolicyTerms(!agreePolicyTerms)}
                             type="checkbox"
                             label='Accept our Privacy Policy and Terms of Service' />
                     </Form.Group>
-                    <Button variant="secondary" type="submit">
+                    <Button
+                        variant="secondary"
+                        type="submit"
+                        disabled={!agreePolicyTerms}
+                    >
                         Register
                     </Button>
                 </Form>
