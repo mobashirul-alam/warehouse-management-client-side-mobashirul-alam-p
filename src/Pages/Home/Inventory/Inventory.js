32a2556341bf2584c238d56b663e3c8603d3b90b
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-05-07 18:55:58 +0600

    detail product shown in home inventory

--- src/Pages/Home/Inventory/Inventory.js
+++ src/Pages/Home/Inventory/Inventory.js
@@ -11,9 +11,9 @@ const Inventory = () => {
             .then(data => setProducts(data))
     }, [])
     return (
-        <div id='inventory'>
-            <h1 className='text-center'>Our Inventory</h1>
-            <div>
+        <div id='inventory' className='container my-5'>
+            <h1 className='text-center mb-5 border-bottom border-secondary w-50 mx-auto pb-2'>Our Inventory</h1>
+            <div className='row row-cols-1 row-cols-md-2 g-4'>
                 {
                     products.splice(0, 6).map(product => <ProductDetails
                         key={product._id}
--- src/Pages/ProductManagement/ProductDetails/ProductDetails.js
+++ src/Pages/ProductManagement/ProductDetails/ProductDetails.js
@@ -4,8 +4,26 @@ const ProductDetails = ({ product }) => {
     const { name, img, description, price, quantity, supplierName } = product;
 
     return (
-        <div>
-            <h1>Product Name : {name}</h1>
+        <div className="card">
+            <div className="row g-0">
+                <div className="col-md-4 my-auto">
+                    <img src={img} className="img-fluid rounded-start" alt="..." />
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">{name}</h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text mb-1">
+                            <span className='fs-5'>Price : </span>
+                            <span className='display-6'>{price}$</span>
+                            <small className='text-muted'>/pcs</small>
+                        </p>
+                        <p className="card-text mb-1 fs-5">In Stock : {quantity} pcs</p>
+                        <p className="card-text"><small className="text-muted">Supplier : {supplierName}</small></p>
+                        <button className='btn btn-secondary'>Stock Update</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
