20a2d8da8ff4aa8063cca43a26a8cb66a3f8afa5
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-05-08 20:12:36 +0600

    adding new item to database

--- src/Pages/ProductManagement/AddItem/AddItem.js
+++ src/Pages/ProductManagement/AddItem/AddItem.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddItem = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        const url = `http://localhost:5000/products`;
+        fetch(url, {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => { })
+    };
+
     return (
         <div className='container'>
             <h1 className='text-center my-4'>Add New Inventory Item</h1>
             <div>
-                <form>
-                    <input className='w-50 my-2 p-1 d-block mx-auto' type="text" placeholder='Product name' />
-                    <input className='w-50 my-2 p-1 d-block mx-auto' type="text" placeholder='Image url' />
-                    <textarea className='w-50 my-2 p-1 d-block mx-auto' name="description" id="" cols="25" rows="2" placeholder='Product description'></textarea>
-                    <input className='w-50 my-2 p-1 d-block mx-auto' type="text" placeholder='Product price' />
-                    <input className='w-50 my-2 p-1 d-block mx-auto' type="number" name="quantity" id="" placeholder='Quantity' />
-                    <input className='w-50 my-2 p-1 d-block mx-auto' type="text" placeholder='Supplier name' />
-                    <input type="submit" value="Add Item" className='w-25 d-block mx-auto mt-3 mb-5 py-1' />
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input className='w-50 my-2 p-1 d-block mx-auto' {...register("name", { required: true })} placeholder='Product name' />
+                    <input className='w-50 my-2 p-1 d-block mx-auto' {...register("img", { required: true })} placeholder='Image url' />
+                    <textarea className='w-50 my-2 p-1 d-block mx-auto' {...register("description", { required: true })} placeholder='Product description' />
+                    <input className='w-50 my-2 p-1 d-block mx-auto' {...register("price", { required: true })} placeholder='Product price' />
+                    <input className='w-50 my-2 p-1 d-block mx-auto' type="number" {...register("quantity", { required: true })} placeholder='Quantity' />
+                    <input className='w-50 my-2 p-1 d-block mx-auto' {...register("supplierName", { required: true })} placeholder='Supplier name' />
+                    <input className='w-25 d-block mx-auto mt-3 mb-5 py-1' type="submit" value="Add Item" />
                 </form>
             </div>
         </div>
