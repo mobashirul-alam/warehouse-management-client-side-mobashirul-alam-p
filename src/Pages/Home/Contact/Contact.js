b3392fb34d21fbce978bb6181a1b5267b7e8d4d7
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-05-08 09:10:54 +0600

    contact with newsletter added

--- src/Pages/Home/Contact/Contact.js
+++ src/Pages/Home/Contact/Contact.js
@@ -2,24 +2,39 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div className='container'>
-            <div>
-                <h3 className='mb-3'>Contact Us</h3>
-                <p>
-                    <i className="fa-solid fa-location-dot"></i>
-                    <span className='fw-bold mx-3'>Address :</span>
-                    221B Baker Street, London, England
-                </p>
-                <p>
-                    <i className="fa-solid fa-phone"></i>
-                    <span className='fw-bold mx-3'>Phone :</span>
-                    1-000-2558-255
-                </p>
-                <p>
-                    <i className="fa-solid fa-envelope"></i>
-                    <span className='fw-bold mx-3'>Email :</span>
-                    ourMail1@wemail.com
-                </p>
+        <div className='bg-primary'>
+            <div className='container'>
+                <div className='row py-5 text-light'>
+                    <div className='col-12 col-md-6'>
+                        <h3 className='mb-3'>Contact Us</h3>
+                        <p>
+                            <i className="fa-solid fa-location-dot"></i>
+                            <span className='fw-bold mx-3'>Address :</span>
+                            221B Baker Street, London, England
+                        </p>
+                        <p>
+                            <i className="fa-solid fa-phone"></i>
+                            <span className='fw-bold mx-3'>Phone :</span>
+                            1-000-2558-255
+                        </p>
+                        <p>
+                            <i className="fa-solid fa-envelope"></i>
+                            <span className='fw-bold mx-3'>Email :</span>
+                            ourMail1@wemail.com
+                        </p>
+                    </div>
+                    <div className='col-12 col-md-6'>
+                        <h3 className='mb-3'>Newsletter</h3>
+                        <p>
+                            By subscribing to our newsletter, you will always be up-to-date about our inventory!
+                        </p>
+                        <input
+                            className='w-75 p-1 mb-2'
+                            type="email" name="email" id="" placeholder='Email Address' />
+                        <br />
+                        <button className='btn btn-warning'>Subscribe</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
