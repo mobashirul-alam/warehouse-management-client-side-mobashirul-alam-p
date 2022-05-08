3c1b51169ccc4673a38f9846692b68309cf8c4d7
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-05-09 00:07:35 +0600

    items loaded with specific user at my items page

--- src/Pages/ProductManagement/AddItem/AddItem.js
+++ src/Pages/ProductManagement/AddItem/AddItem.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import auth from '../../../firebase.init';
 
 const AddItem = () => {
+    const [user] = useAuthState(auth);
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => {
+    const onSubmit = (data, e) => {
+        console.log(data)
         const url = `http://localhost:5000/products`;
         fetch(url, {
             method: "POST",
@@ -13,7 +18,10 @@ const AddItem = () => {
             body: JSON.stringify(data)
         })
             .then(res => res.json())
-            .then(result => { })
+            .then(result => {
+                toast('Items Added Successfully')
+                e.target.reset();
+            })
     };
 
     return (
@@ -21,6 +29,7 @@ const AddItem = () => {
             <h1 className='text-center my-4'>Add New Inventory Item</h1>
             <div>
                 <form onSubmit={handleSubmit(onSubmit)}>
+                    <input className='w-50 my-2 p-1 d-block mx-auto' value={user.email} {...register("email", { required: true })} placeholder='User email' />
                     <input className='w-50 my-2 p-1 d-block mx-auto' {...register("name", { required: true })} placeholder='Product name' />
                     <input className='w-50 my-2 p-1 d-block mx-auto' {...register("img", { required: true })} placeholder='Image url' />
                     <textarea className='w-50 my-2 p-1 d-block mx-auto' {...register("description", { required: true })} placeholder='Product description' />
--- src/Pages/ProductManagement/MyItems/MyItems.js
+++ src/Pages/ProductManagement/MyItems/MyItems.js
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const MyItems = () => {
+    const [user] = useAuthState(auth);
+    const [myProducts, setMyProducts] = useState([]);
+
+    useEffect(() => {
+        const url = `http://localhost:5000/userProduct?email=${user.email}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setMyProducts(data))
+    }, [user]);
+
+    // const { _id, name, price, quantity, supplierName } = myProducts;
+
+    const handleDeleteItem = id => {
+        const proceed = window.confirm('Are you sure to delete ?');
+        if (proceed) {
+            const url = `http://localhost:5000/product/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        const remainingItems = myProducts.filter(product => product._id !== id);
+                        setMyProducts(remainingItems);
+                    }
+                })
+        }
+    }
+
     return (
-        <div>
-            <h1>My items</h1>
+        <div className='container'>
+            <h3 className='text-center my-4'>My Items</h3>
+            <div className='row'>
+                {
+                    myProducts.map(product => {
+                        return <div key={product._id} className='col-12 col-sm-6 col-md-4'>
+                            <div className="card border-success mb-3">
+                                <div className="card-header bg-transparent border-success">
+                                    <h5 className="card-title">{product.name}</h5>
+                                </div>
+                                <div className="card-body text-success">
+                                    <p className="card-text">
+                                        Price : {product.price} <small className='text-muted'>$/pcs</small>
+                                    </p>
+                                    <p className="card-text">
+                                        Quantity : {product.quantity} pcs
+                                    </p>
+                                    <p className="card-text">
+                                        Supplier : {product.supplierName}
+                                    </p>
+                                </div>
+                                <div className="card-footer bg-transparent border-success">
+                                    <button onClick={() => handleDeleteItem(product._id)}
+                                        className='btn btn-secondary w-100'>
+                                        Delete Item
+                                    </button>
+                                </div>
+                            </div>
+                        </div>;
+                    })
+                }
+            </div>
         </div>
     );
 };
